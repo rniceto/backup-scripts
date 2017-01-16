77e2edb3250c045d28251c9a00308973847f50e6
Rodolpho Niceto <rodolphoniceto@yahoo.com.br>
2017-01-16 14:07:35 -0200

    Little changes

--- backup-dayli.sh
+++ backup-dayli.sh
@@ -2,13 +2,13 @@
 
 DATE=`date "+%d-%m-%Y"`
 KVERSION=`uname -r`
-BACKUPDIR=/admrede/backups
+BACKUPDIR=/home/admin/backups
 
 # Remove backup's antigos superiores a 10 dias
 
 find $BACKUPDIR -type f -mtime +10 -delete
 
-dpkg --get-selections > $BACKUPDIR/$date-"server-packages   #Mostra todas biblitotecas instaladas
-tar -cvzf /admrede/backups/$date-“Name Server”.tgz /etc
+#######dpkg --get-selections > $BACKUPDIR/$date-"server-packages   #Mostra todas biblitotecas instaladas
+tar -cvzf /home/admin/backups/$date-“Name Server”.tgz /etc
 
 
